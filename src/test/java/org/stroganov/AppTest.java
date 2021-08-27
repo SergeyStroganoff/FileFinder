abd9fe15f87f309075ff6c393b401b3762a23c6e
Sergey Stroganov <111sergey90@gmail.com>
2021-08-27 16:08:12 +0300

    Initial commit

---
+++ src/test/java/org/stroganov/AppTest.java
@@ -0,0 +1,20 @@
+package org.stroganov;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    void shouldAnswerWithTrue() {
+        assertEquals(12,12);
+    }
+}

332960db4cd9a52df74c8893f93ac709447082b7
Sergei Stroganov <sergei_stroganov@epam.com>
2021-08-31 22:53:48 +0300

    Added test for FileFinder method

---
+++ src/test/java/org/stroganov/impl/FileFinderTest.java
@@ -0,0 +1,13 @@
+package org.stroganov.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileFinderTest {
+
+    @Test
+    void getFilesPathListByPartName() {
+        assertTrue(true);
+    }
+}
